f98ae73938aa6d22f8c30b262ab2a01cf42ea2ad
Marc TITINGER <m.titinger@vogo-group.com>
2022-10-04 23:13:42 +0200

    use vscode multi-root feature, so user can select between debig configs
    Signed-off-by: Marc TITINGER <m.titinger@vogo-group.com>

--- primitives/src/main.rs
+++ primitives/src/main.rs
@@ -2,9 +2,10 @@
 
 #[allow(unused)]
 fn main() {
-    // Hello
-    println!("Hello, world!");
+    /* scalar types */
+    let logical: bool = true;
+    let my_short: i16 = 123;
+    let your_long: i32 = -2000000000;
+    let my_dword: u32 = 4000000000;
 
-    // Stepping in the random crate
-   // let x: u8 = random();
 }
